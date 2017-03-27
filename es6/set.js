998809e90b1b6a9031d2b0bcf72b04568abf0986
gpd <guopeidong@idcool.com.cn>
2017-03-27 16:27:52 +0800

    exercise es6-set

---
+++ es6/set.js
@@ -0,0 +1,29 @@
+//set 数据结构
+const s=new Set();
+[2,3,4,5,2,3].forEach((x=>s.add(x)));
+
+for(let i of s){
+	console.log(i);
+}
+
+let set=new Set([1,2,3,4,5,1,2]);
+console.log([...set]);
+
+console.log(NaN===NaN);
+set.add(NaN);
+set.add(NaN);
+console.log(set);
+console.log(set.constructor);
+console.log(set.has(NaN));
+//console.log(set.keys()+"\n"+set.values()+"\n"+set.entries());//iterator
+for(let item of set.keys()){
+	console.log(item);
+}
+
+for(let item of set.values()){
+	console.log(item);
+}
+
+for(let item of set.entries()){
+	console.log(item);
+}
