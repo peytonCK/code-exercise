da4a08f7b78aa210e3978ce68ea834f6428447b7
gpd <guopeidong@idcool.com.cn>
2017-04-11 16:55:42 +0800

    modify regex code

--- regex-node/textToHtml.js
+++ regex-node/textToHtml.js
@@ -1,5 +1,6 @@
 //把一个纯文本转成html
-var text = ''; //待输入
+var text = "<1>这是个测试\n guopeidong@idcool.com.cn 是我的邮箱\n 我的主站网址是 http://guopeidong.com "; //待输入
+console.log(text);
 
 //1、处理特殊符号 &=>&amp >=>&gt; <=>&lt;
 text = text.replace(/&/g, '&amp;');
@@ -12,4 +13,11 @@ text = text.replace(/^\s*$/mg, '<p/>');
 //3、将email地址转换成超链接形式
 text = text.replace(/\b\w[-.\w]*\@\w+(\.\w+)+\b/g, function(match) {
 	return "<a mailto='" + match + "'>" + match + "</a>";
-})
+});
+
+//4、将HTTP URL转换成链接形式
+text = text.replace(/\bhttp(s)?:\/\/[-a-z0-9]+(\.[-a-z0-9]+)*\.(com|edu|info)(\/[-a-z0-9_:\@&?=+,.!/~*'%\$']*)?\b/g, function(match) {
+	return "<a href='" + match + "'>" + match + "</a>";
+});
+
+console.log(text);
